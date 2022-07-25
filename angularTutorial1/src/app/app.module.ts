a892af58fb7ceffdd86ca81cea96e14a0fada5f4
DilakshiFernando <68247015+Manushi1997@users.noreply.github.com>
2022-07-25 11:17:56 +0530

    add

---
+++ angularTutorial1/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { LkrFormatterPipe } from './shared/lkr-formatter.pipe';
+import { ProgressBarComponent } from './shared/progress-bar/progress-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeesComponent,
+    LkrFormatterPipe,
+    ProgressBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
