48e1b946711a3c347c7506f7485248d1acdccef1
DilakshiFernando <68247015+Manushi1997@users.noreply.github.com>
2022-07-25 11:04:01 +0530

    add

---
+++ AssignmentsABC/src/B/B.java
@@ -0,0 +1,10 @@
+package B;
+import A.A;
+import C.C;
+
+public class B extends A{
+    public void run()
+    {
+        System.out.println("B is running...");
+    }
+}
---
+++ AssignmentsABC/src/C/C.java
@@ -0,0 +1,12 @@
+package C;
+import B.B;
+import A.A;
+
+
+public class C {
+    public static void main(String args[])
+    {
+        B obj1 = new B();
+        obj1.run();
+    }
+}
