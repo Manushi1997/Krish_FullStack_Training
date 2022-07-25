6f59076344e58aac33d5afc502df2ce3561af899
DilakshiFernando <68247015+Manushi1997@users.noreply.github.com>
2022-07-25 11:09:44 +0530

    add springboot T2

---
+++ SpringbootT2/src/test/java/com/dilakshi/AppTest.java
@@ -0,0 +1,20 @@
+package com.dilakshi;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
