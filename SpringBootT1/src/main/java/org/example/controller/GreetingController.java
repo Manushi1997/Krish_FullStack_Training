2e2d5ff90b2a827b994e0ee02d285841baecb35a
DilakshiFernando <68247015+Manushi1997@users.noreply.github.com>
2022-07-25 11:09:19 +0530

    add springboot T1

---
+++ SpringBootT1/src/main/java/org/example/controller/GreetingController.java
@@ -0,0 +1,12 @@
+package org.example.controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @RequestMapping("/")
+    public String greeting(){
+        return "<h1> Welcome to game changing framework.. Spring Boot!! </h1>";
+    }
+}
